1fddc6776c9bdab5a8c801ec309359b630ae513c
Alejandro Zárate <alejandro.zarate.contact@gmail.com>
2024-03-12 13:57:14 -0500

    Update cartsRouter.js

--- routes/cartsRouter.js
+++ routes/cartsRouter.js
@@ -2,25 +2,11 @@
 
 const express = require('express');
 const router = express.Router();
-
-// Importar el controlador de carritos
 const cartsController = require('../controllers/cartsController');
 
-// Ruta para crear un nuevo carrito
 router.post('/', (req, res) => {
     const newCart = cartsController.createCart();
     res.status(201).json(newCart);
-});
-
-// Ruta para obtener un carrito por su ID
-router.get('/:cid', (req, res) => {
-    const cartId = parseInt(req.params.cid);
-    const cart = cartsController.getCartById(cartId);
-    if (cart) {
-        res.json(cart);
-    } else {
-        res.status(404).json({ error: 'Carrito no encontrado' });
-    }
 });
 
 module.exports = router;
